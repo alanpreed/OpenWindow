5ef39f1da7686385ef5206e633982a99ade17564
Alan Reed <alan_p_reed@hotmail.co.uk>
2020-09-29 15:43:10 +0100

    Simplified so that TX interrupt is always enabled

--- Software/EFM8OpenWindow/src/UART1.c
+++ Software/EFM8OpenWindow/src/UART1.c
@@ -41,20 +41,17 @@ void UART1_write(char c) {
 
 // Public functions
 void UART1_init(void) {
+  // Enable transmit completion interrupt
+  uint8_t old_page = SFRPAGE;
+  SFRPAGE = UART1_SFR_PAGE;
+  UART1FCN1_TIE = 1;
+  SFRPAGE = old_page;
+
   transmit_buffer.read_index = 0;
   transmit_buffer.write_index = 0;
   receive_buffer.read_index = 0;
   receive_buffer.read_index = 0;
   transmitting = false;
-}
-
-void UART1_set_tx_interrupt(bool value) {
-  uint8_t old_page = SFRPAGE;
-  transmitting = value;
-
-  SFRPAGE = UART1_SFR_PAGE;
-  UART1FCN1_TIE = value;
-  SFRPAGE = old_page;
 }
 
 char putchar(char c) {
@@ -67,11 +64,10 @@ char putchar(char c) {
     success = buffer_get(&transmit_buffer, &value);
 
     if (success) {
-      UART1_set_tx_interrupt(true);
+      transmitting = true;
       UART1_write(value);
     }
   }
-
   return c;
 }
 
@@ -86,7 +82,7 @@ void UART1_tx_callback(void) {
     UART1_write(value);
   }
   else {
-    UART1_set_tx_interrupt(false);
+    transmitting = false;
   }
 }
 
